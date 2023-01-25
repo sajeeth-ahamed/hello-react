887de9e1f5ce483813db4471a01a969422123a6d
Sajeeth Ahmad <sajiiahd@gmail.com>
2023-01-25 22:23:04 +0530

    fix: modify some content

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello ReactJS!</h1>
+        <h2>GitHub Actions Testing!!!</h2>
       </header>
     </div>
   );
