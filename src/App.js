4ad3dac40a8a1f61c5a6f23c35e1979f852dc0e7
Sanjay <sanjaykirupaharan21@gmail.com>
2023-01-28 10:38:35 +0530

    fix: clean up

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello ReactJS!</h1>
+        <h1>Hello ReactJS! 2</h1>
         <h2>GitHub Actions Testing</h2>
         <h2>Test at DSA</h2>
         <h2>Test at DSA 2</h2>
